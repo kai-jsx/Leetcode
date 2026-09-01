1b8520f076899f8e9e6ae6220366cb9ba91c4fa0
Eagar Nandi <101915881+kai-jsx@users.noreply.github.com>
2026-09-01 13:11:39 +0530

    Time: 0 ms (100%), Space: 9.1 MB (44.72%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if (haystack.size() < needle.size())
-            return -1;
-        for (int i = 0; i < haystack.size(); i++){
-            if (haystack[i] == needle[0]){
-                if (needle == haystack.substr(i,needle.size())){
-                    return i;
-                }
+        int n = haystack.size();
+        int m = needle.size();
+
+        if (m > n) return -1;
+
+        for (int i = 0; i <= n - m; i++) {
+            // Compare m characters of haystack starting at index i with needle directly (No heap memory allocation)
+            if (haystack.compare(i, m, needle) == 0) {
+                return i;
             }
         }
         return -1;
