9576071d4dcd16d9414e99854443e08a36428458
Eagar Nandi <101915881+kai-jsx@users.noreply.github.com>
2026-09-12 14:49:02 +0530

    Time: 0 ms (100%), Space: 11.9 MB (59.13%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string sol = "";
+        // You don't even need a 'sol' variable!
         string first = strs[0];
-        for( int i = 0 ; i < first.length(); i++){
-            int x = 1;
-            for (int j = 1; j < strs.size(); j++){
-                string next = strs[j];
-                if(first[i] != next[i])
-                    x = 0;
+        
+        for (int i = 0; i < first.length(); i++) {
+            
+            for (int j = 1; j < strs.size(); j++) {
+                // Check 1: Is the next word too short? (Prevents crash)
+                // Check 2: Do the characters mismatch?
+                if (i == strs[j].length() || first[i] != strs[j][i]) {
+                    
+                    // Slice 'first' from index 0, taking 'i' characters
+                    return first.substr(0, i); 
+                }
             }
-            if (x == 1)
-                sol += first[i];
-            else
-                break;
         }
-         return (sol); }
+        
+        // If we checked every letter and never broke out, 
+        // the entire first word is the common prefix.
+        return first; 
+    }
 };
